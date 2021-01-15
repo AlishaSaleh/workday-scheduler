0e92b499592307c69677b86aeea35e848947fed0
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-01-15 21:24:23 +0000

    textarea updates/time colour fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+var tasksObj;
+
 
 $(document).ready(function () {
     var currentDay = $("#currentDay");
@@ -7,44 +9,122 @@ $(document).ready(function () {
 
     currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-    var currentHour = moment().format('LT');
+    var currentHour = parseInt(moment().format('H'));
 
     console.log(currentHour);
 
+    tasksObj = {
+        "9" : "",
+        "10" : "",
+        "11" : "",
+        "12" : "",
+        "13" : "",
+        "14" : "",
+        "15" : "",
+        "16" : "",
+        "17" : ""
+    };
+
+    function storeTasks () {
+        localStorage.setItem("tasksObj", JSON.stringify(tasksObj));
+
+    };
+
+    function retriveTasks () {
+        var loadTasks = JSON.parse(localStorage.getItem("tasks"));
+        // if (loadTasks) {
+        //     tasksObj = loadTasks;
+
+        //     $.each(tasksObj, function (hour, task) {
+        //         var hourDiv = $("#" + hour);
+                
+        //     });
+        
+        };
+
+    // });
+
     
 
-    styleTasks();
+    
 
     function styleTasks() {
 
 
         $(".row-info").each(function () {
-            var elementHour = parseInt($(this).attr("id"));
+            var elementHour = parseInt(($(this).attr("id")));
+            console.log("element hour",elementHour);
+            console.log("current hour",currentHour);
             if (elementHour < currentHour) {
-                $(this).removeClass(["present", "future"]).addClass("past");
-                // $(this).addClass("past");
+                // $(this).removeClass(["present", "future"]).addClass("past");
+                $(this).addClass("past");
             }
             else if (elementHour === currentHour) {
-                $(this).removeClass(["past", "future"]).addClass("present");
-                // $(this).addClass("present");
+                // $(this).removeClass(["past", "future"]).addClass("present");
+                $(this).addClass("present");
             }
             else {
-                $(this).removeClass(["past", "present"]).addClass("future");
-                // $(this).addClass("future");
+                // $(this).removeClass(["past", "present"]).addClass("future");
+                $(this).addClass("future");
 
-            }
-        })
-    }
+            };
+        });
+    };
 
-    function addText() {
-        var taskInfo = $(".task")
-    }
+    // $(".task").on("click", function() {
+    //     var taskInfo = $(".task");
 
-    $(".task").on("click", function inputText() {
-       $("textarea").each(function() {
-           addText($(this));
-       })
+    //     var inputField = $("<input>");
+
+    //     inputField.addClass("description");
+
+    //     taskInfo.append(inputField);
+
+    // });
+
+    // 
+
+    $("button").on("click", function() {
+        console.log($(this).parent().attr("id"));
+        var taskId = $(this).parent().attr("id");
+        // get id
+        // get textarea input
+        var textInput = $(this).parent().find("textarea");
+        textInput.val();
+        console.log(textInput.val());
+
+        // populate the obj in the background
+        
+        tasksObj[taskId] = textInput.val();
+
+
+        // trigger a save function 
     })
+
+    // function addText() {
+    //     var taskInfo = $(".task");
+
+    //     var inputField = $("<input>");
+
+    //     inputField.addClass("description");
+
+    //     taskInfo.append(inputField);
+
+
+
+        
+    // }
+
+    // $(".task").on("click", function inputText(event) {
+    //    $(this).event.target(function() {
+    //        addText($(this));
+    //    });
+    // });
+
+
+    styleTasks();
+    storeTasks();
+    console.log(tasksObj);
     // schedCont.empty();
 
     // for (var hour = 9; hour <= 17; hour++);
